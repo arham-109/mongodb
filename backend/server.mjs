6477fed7012b82d9b7d75a40dfa46857c668c358
arham-109 <arhamshafiq2007@gmail.com>
2026-08-16 03:58:41 +0500

    updated

--- backend/server.mjs
+++ backend/server.mjs
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1", postRoutes);
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
   database_connect();
 });
